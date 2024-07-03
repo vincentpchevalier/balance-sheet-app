e2471325c59d80c2baec23a33ff0efd8866c492f
vincentpchevalier <vincentpchevalier@proton.me>
2024-07-03 13:27:23 -0400

    add fieldsets to form for organization

--- src/TrackingForm.jsx
+++ src/TrackingForm.jsx
@@ -32,40 +32,52 @@ function TrackingForm({ onAddEntry }) {
 		<div>
 			<h2>Track Your Income/Expenses</h2>
 			<form id="trackingForm" onSubmit={handleSubmit}>
-				<input
-					type="date"
-					name="date"
-					id="date"
-					onChange={(ev) => setDate(ev.target.value)}
-				/>
-				<input
-					type="text"
-					name="description"
-					placeholder="Description"
-					onChange={(ev) => setDescription(ev.target.value)}
-				/>
-				<input
-					type="text"
-					name="amount"
-					placeholder="Amount"
-					onChange={(ev) => setAmount(+ev.target.value)}
-				/>
-				<select
-					name="category"
-					id="category-select"
-					value={category}
-					onChange={(ev) => setCategory(ev.target.value)}
-				>
-					<option value="Food">Food</option>
-					<option value="Housing">Housing</option>
-					<option value="Job">Job</option>
-					<option value="Utilities">Utilities</option>
-					<option value="Transportation">Transportation</option>
-					<option value="Healthcare">Healthcare</option>
-					<option value="Insurance">Insurance</option>
-					<option value="Entertainment">Entertainment</option>
-					<option value="Other">Other</option>
-				</select>
+				<fieldset>
+					<label htmlFor="date">Date</label>
+					<input
+						type="date"
+						name="date"
+						id="date"
+						onChange={(ev) => setDate(ev.target.value)}
+					/>
+				</fieldset>
+				<fieldset>
+					<label htmlFor="description">Description</label>
+					<input
+						type="text"
+						name="description"
+						placeholder="Description"
+						onChange={(ev) => setDescription(ev.target.value)}
+					/>
+				</fieldset>
+				<fieldset>
+					<label htmlFor="amount">Amount ($)</label>
+					<input
+						type="text"
+						name="amount"
+						placeholder="Amount"
+						onChange={(ev) => setAmount(+ev.target.value)}
+					/>
+				</fieldset>
+				<fieldset>
+					<label htmlFor="category">Category</label>
+					<select
+						name="category"
+						id="category-select"
+						value={category}
+						onChange={(ev) => setCategory(ev.target.value)}
+					>
+						<option value="Food">Food</option>
+						<option value="Housing">Housing</option>
+						<option value="Job">Job</option>
+						<option value="Utilities">Utilities</option>
+						<option value="Transportation">Transportation</option>
+						<option value="Healthcare">Healthcare</option>
+						<option value="Insurance">Insurance</option>
+						<option value="Entertainment">Entertainment</option>
+						<option value="Other">Other</option>
+					</select>
+				</fieldset>
 				<button>Submit</button>
 			</form>
 		</div>
