34355e65d655eb3ab5d7745564423f831b88659c
vincentpchevalier <vincentpchevalier@proton.me>
2024-07-02 22:12:04 -0400

    set up initial entry data and pass down into BalanceSheet

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,30 @@ import { useState } from 'react';
 import './App.css';
 import BalanceSheet from './BalanceSheet';
 
+const initialEntries = [
+	{
+		source: 'Salary',
+		category: 'Job',
+		balance: 1500,
+	},
+	{
+		source: 'Rent',
+		category: 'Housing',
+		balance: -500,
+	},
+	{
+		source: 'Groceries',
+		category: 'Food',
+		balance: -75,
+	},
+];
+
 function App() {
+	const [entries, setEntries] = useState(initialEntries);
 	return (
 		<div className="App">
 			<h1>Balance Sheet</h1>
-			<BalanceSheet />
+			<BalanceSheet entries={entries} />
 		</div>
 	);
 }
